18eb4ed8d8f4b676d106454a2438ccc71035653b
Josey Hanish <josey.hanish@gmail.com>
2017-11-07 13:39:11 -0600

    plotting the frequency spectrum

--- snr.py
+++ snr.py
@@ -7,6 +7,7 @@ Created on Tue Oct 24 13:45:44 2017
 
 import numpy as np
 from readligo import loaddata
+import matplotlib.pyplot as plt
 
 def snr_old(signal):
     print np.amax(signal)
@@ -15,6 +16,7 @@ def snr_old(signal):
         np.std(signal[:signal.index(np.amax(signal))-3]+signal[signal.index(np.amax(signal))+3:])
 
 def snr(strain_data):
+    "Don't use this; I need to fix it."
     fourier_domain_strain = list(np.absolute(np.fft.fft(strain_data)))
     print np.amax(fourier_domain_strain)
     print np.std(fourier_domain_strain[:fourier_domain_strain.index(np.amax(fourier_domain_strain))-3]\
@@ -22,6 +24,20 @@ def snr(strain_data):
     return np.amax(fourier_domain_strain)/ \
         np.std(fourier_domain_strain[:fourier_domain_strain.index(np.amax(fourier_domain_strain))-3]\
         +fourier_domain_strain[fourier_domain_strain.index(np.amax(fourier_domain_strain))+3:])
+
+def plot_frequency_spectrum(strain_data, dt):
+    fourier = np.fft.rfft(strain_data)
+    print np.shape(fourier)
+    amplitudes = np.absolute(fourier)
+    freqs = np.fft.rfftfreq(len(strain_data), dt)
+    plt.figure(figsize=(10,10))
+    plt.plot(freqs[1:], amplitudes[1:])
+#    plt.xlim(0, 30)
+    plt.show()
+    
+#    print freqs[1] - freqs[0]
+    plt.figure(figsize=(10,10))
+    plt.plot(freqs[500:], amplitudes[500:])
     
 if __name__ == '__main__':
 #    test = [0,1,0,1,0,1,3,4,5,4,3,0,1,0,1,0]
@@ -32,7 +48,12 @@ if __name__ == '__main__':
     if strain is None or time is None or chan_dict is None:
         print 'Loading data failed'
         quit()
-#    print 'len(strain) = {}'.format(len(strain))
-#    print 'len(time) = {}'.format(len(time))
-    print snr(strain)
+    print 'len(strain) = {}'.format(len(strain))
+    print 'len(time) = {}'.format(len(time))
+    dt = time[1] - time[0]
+    
+#    print snr(strain)
+    
+    print '\n'
+    plot_frequency_spectrum(strain, dt)
     
